e1be3a044db7ad30693e7437685540c2f79de931
Maciej Ślusarz <maciej.slusarz@proton.me>
2023-06-05 22:03:26 +0200

    Add comments to linked_list.cpp informing about steps done in each method

--- src/linked_list.cpp
+++ src/linked_list.cpp
@@ -1,8 +1,12 @@
 #include "linked_list.h"
 
+/**
+ * Clean up list, if destructed
+ */
 LinkedList::~LinkedList() {
     auto next = _first->_next;
     delete _first;
+    // Going through elements to destruct each one
     while (next != nullptr) {
         auto to_free = next;
         next = next->_next;
@@ -12,10 +16,12 @@ LinkedList::~LinkedList() {
 
 void LinkedList::append(int value) {
     auto element = this->_first;
+    // Add element to list, if empty
     if (element == nullptr) {
         _first = new ListNode(value);
         return;
     }
+    // Get next element unless we found null
     while (element->_next != nullptr) {
         element = element->_next;
     }
@@ -31,6 +37,7 @@ int LinkedList::get(int index) {
     while (i < index) {
         ++i;
         element = element->_next;
+        // If we're out of bounds, then throw exception, no sense to going any further
         if (element == nullptr)
             throw std::out_of_range("List index out of range");
     }
@@ -45,11 +52,14 @@ void LinkedList::remove(int index) {
     while (i < index - 1) {
         ++i;
         element = element->_next;
+        // If we're out of bounds, then throw exception, no sense to going any further
         if (element == nullptr)
             throw std::out_of_range("List index out of range");
     }
     auto one_before = element;
+    // Apply to element pointer, that one to remove (according to index)
     element = one_before->_next;
+    // If we're out of bounds, then throw exception, no sense to going any further
     if (element == nullptr)
         throw std::out_of_range("List index out of range");
     if (element->_next != nullptr) {
@@ -59,10 +69,12 @@ void LinkedList::remove(int index) {
     delete element;
 }
 
+// TODO: Implement
 void LinkedList::pop() {
 
 }
 
+// TODO: Implement
 size_t LinkedList::size() {
     return 0;
 }
