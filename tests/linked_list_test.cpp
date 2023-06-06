db9b8a85c69195805b1503d83a4cba8fe57dfccb
Maciej Ślusarz <maciej.slusarz@proton.me>
2023-06-06 19:54:07 +0200

    Prepare first tests for size(), get() and remove() methods of LinkedList instances

--- tests/linked_list_test.cpp
+++ tests/linked_list_test.cpp
@@ -1,5 +1,27 @@
 #include <gtest/gtest.h>
+#include "linked_list.h"
 
 TEST(LinkedListTest, Size) {
-    EXPECT_EQ(2 * 2, 4);
+    LinkedList linkedList;
+    linkedList.append(2);
+    linkedList.append(3);
+    linkedList.append(5);
+
+    ASSERT_EQ(linkedList.size(), 3);
+}
+
+TEST(LinkedListTest, Get) {
+    LinkedList linkedList;
+    linkedList.append(1);
+
+    ASSERT_EQ(linkedList.get(0), 1);
+}
+
+TEST(LinkedListTest, Remove) {
+    LinkedList linkedList;
+    linkedList.append(2);
+    //linkedList.append(3);
+    linkedList.remove(0);
+
+    ASSERT_EQ(linkedList.size(), 0);
 }
