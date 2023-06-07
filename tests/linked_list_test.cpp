eb57567f49cb70f9d0e89ceaff2229d92e755586
Maciej Ślusarz <maciej.slusarz@proton.me>
2023-06-07 18:37:31 +0200

    Add Append and Pop test cases

--- tests/linked_list_test.cpp
+++ tests/linked_list_test.cpp
@@ -1,6 +1,14 @@
 #include <gtest/gtest.h>
 #include "linked_list.h"
 
+
+/**
+ * TODO: Make parametrized test of size method including:
+ * 1. Size when list is empty
+ * 2. Size when list has one element
+ * 3. Size when more elements
+ * Scenarios which can help with memory leaks and segmentation faults
+ */
 TEST(LinkedListTest, Size) {
     LinkedList linkedList;
     linkedList.append(2);
@@ -10,6 +18,13 @@ TEST(LinkedListTest, Size) {
     ASSERT_EQ(linkedList.size(), 3);
 }
 
+/**
+ * TODO: Make parametrized test of get method including:
+ * 1. Getting from the beginning
+ * 2. Getting between beginning and end
+ * 3. Getting at the end
+ * Scenarios which can help with memory leaks and segmentation faults
+ */
 TEST(LinkedListTest, Get) {
     LinkedList linkedList;
     linkedList.append(1);
@@ -17,6 +32,13 @@ TEST(LinkedListTest, Get) {
     ASSERT_EQ(linkedList.get(0), 1);
 }
 
+/**
+ * TODO: Make parametrized test of remove method including:
+ * 1. Removing from the beginning
+ * 2. Removing between beginning and end
+ * 3. Removing at the end
+ * Scenarios which can help with memory leaks and segmentation faults
+ */
 TEST(LinkedListTest, Remove) {
     LinkedList linkedList;
     linkedList.append(2);
@@ -24,4 +46,22 @@ TEST(LinkedListTest, Remove) {
     linkedList.remove(0);
 
     ASSERT_EQ(linkedList.size(), 0);
+}
+
+TEST(LinkedListTest, Pop) {
+    LinkedList linkedList;
+    linkedList.append(5);
+    linkedList.append(3);
+    linkedList.append(1);
+
+    linkedList.pop();
+
+    EXPECT_ANY_THROW(linkedList.get(2));
+}
+
+TEST(LinkedListTest, Append) {
+    LinkedList linkedList;
+    linkedList.append(2);
+
+    EXPECT_EQ(linkedList.get(0), 2);
 }
